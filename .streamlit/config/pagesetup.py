30d09804b238600263100a08e8b2b6edacfddb26
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-04-27 18:39:04 -0500

    Create pagesetup.py

---
+++ .streamlit/config/pagesetup.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def set_title(varTitle, varSubtitle):
+        with stylable_container(
+                key="container_with_border",
+                css_styles=["""
+                    {
+                        border: 1px solid rgba(115, 0, 0, 1);
+                        background-color: rgba(115, 0, 0, .75);
+                       
+                        
+                    }
+                    """,
+                    """
+                    .stMarkdown {
+                            padding-right: .2em;
+                            padding-left: .5em;
+                        """]
+                ):
+                        st.markdown(f"""<span style="font-weight: bold; color:#0096D7; font-size:1.3em;">{varSubtitle}</span>""", unsafe_allow_html=True)
+                        st.divider()
