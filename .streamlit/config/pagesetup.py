8e47964523682a890b6e3f10c4fb30ec5212713a
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-04-27 19:40:46 -0500

    Update pagesetup.py

--- .streamlit/config/pagesetup.py
+++ .streamlit/config/pagesetup.py
@@ -27,19 +27,21 @@ def set_title(varTitle, varSubtitle):
 
 
 
-
 #Set Background Image
-def display_background_image():
-    # Set the Streamlit image for branding as the background with transparency
-    background_image = 'https://storage.googleapis.com/production-domaincom-v1-0-8/048/1724048/4RBifvGs/dfc737c8f0d640cfa7e8623583bfcf5e'
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.90)), url({background_image});
-            background-size: cover;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+
+# URL of the image
+bg_image_url = "https://raw.githubusercontent.com/jbeduze/MSU-Wrestling/main/.streamlit/config/e52cca5a-1264-4351-ac5e-ad48bd46ea9b.webp
+"
+
+# CSS to inject contained in a string
+bg_style = f"""
+<style>
+body {{
+background-image: url("{bg_image_url}");
+background-size: cover;
+}}
+</style>
+"""
+
+# Inject CSS with Markdown
+st.markdown(bg_style, unsafe_allow_html=True)
